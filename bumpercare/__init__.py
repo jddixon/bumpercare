8214ec73c9ba2b92490945cd3a60c6f210ea5c16
Jim Dixon <jddixon@gmail.com>
2017-04-18 15:42:39 -0700

    drop dist/ ; minor edits

--- bumpercare/__init__.py
+++ bumpercare/__init__.py
@@ -2,7 +2,7 @@
 
 """ default proj desc """
 
-__version__ = '0.0.2'
-__version_date__ = '2017-04-06'
+__version__ = '0.0.3'
+__version_date__ = '2017-04-18'
 
 __all__ = ['__version__', '__version_date__']
