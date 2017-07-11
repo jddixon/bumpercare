6dc3c670c05b502745ea35809c556d042b2bebbe
Jim Dixon <jddixon@gmail.com>
2017-07-11 11:52:34 -0700

    add MANIFEST.in, rewrite setup.py, minor edits to config files

--- src/bumpercare/__init__.py
+++ src/bumpercare/__init__.py
@@ -2,7 +2,7 @@
 
 """ default proj desc """
 
-__version__ = '0.0.3'
-__version_date__ = '2017-06-07'
+__version__ = '0.0.4'
+__version_date__ = '2017-07-11'
 
 __all__ = ['__version__', '__version_date__']
